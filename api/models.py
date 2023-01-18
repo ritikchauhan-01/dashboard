016fe29bb0a7fdf9b85bdf6818eee78b94313195
root <root@DESKTOP-GBSM13H>
2023-01-18 11:21:06 +0530

    adding some minor changes in dashboard views

--- api/models.py
+++ api/models.py
@@ -3,11 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    Internal = 0
-    External = 1
     DEPARTMENT_CHOICES = (
-        (External, ' External'),
-        (Internal, 'Internal'),
+        ("External", 'External'),
+        ("Internal", 'Internal'),
     )
     employee_type = models.CharField(max_length=10, choices=DEPARTMENT_CHOICES)
     salary = models.IntegerField(default=False, null=False)
