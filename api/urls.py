3a3d1a35f6553b37ca842577b105451b71c5355f
root <root@DESKTOP-GBSM13H>
2023-01-20 11:25:39 +0530

    add the Readme.md file and change moved the dashboard url to homepage

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import RegisterationView, LoginView, LogoutView, UserView
-from dashboard.views import DashBoardView
 
 
 urlpatterns = [
-    path('dashboard/', DashBoardView.as_view()),
     path('register', RegisterationView),
     path('login', LoginView),
     path('logout', LogoutView),
